8643a941ed5d2fb022ba53f940fb0bd03d510f99
min <min.q@kakaobank.com>
2022-10-14 08:42:19 +0900

    oss 예제 코드 수정

--- lib/oss/oss_example.dart
+++ lib/oss/oss_example.dart
@@ -1,34 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_package_example/oss_licenses.dart';
 
-class OssScreen extends StatelessWidget {
+class OssScreen extends StatefulWidget {
   const OssScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<OssScreen> createState() => _OssScreenState();
+}
 
+class _OssScreenState extends State<OssScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: SafeArea(
+          top: true,
+          left: false,
+          right: false,
+          bottom: false,
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              centerTitle: true,
+              backgroundColor: Colors.white,
+              title: Text(
+                '라이센스',
+              ),
+            ),
+            body: Column(
+              children: ossLicenses.map((package) {
+                return OssWidget(
+                  package: package,
+                );
+              }).toList(),
+            ),
+          )),
+    );
+  }
+}
+
+class OssWidget extends StatelessWidget {
+  final Package package;
+
+  const OssWidget({
+    Key? key,
+    required this.package,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
-      children: ossLicenses.map((oss) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Text(oss.name),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              margin: const EdgeInsets.only(bottom: 20),
-              child: Text(oss.description),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            if (oss.license != null) Text(oss.license!),
-          ],
-        );
-      }).toList(),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Text(package.name),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Text(package.description),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        if (package.license != null) Text(package.license!),
+      ],
     );
   }
 }
